f241ade6223bdd712df828d3925a23b9b2d7984d
SKUM149 <Saurav.Kumar@albertsons.com>
2023-04-26 11:36:15 +0530

    upadated

--- src/main/java/com/sapient/grooming/TopicBased/MultiThreading.java
+++ src/main/java/com/sapient/grooming/TopicBased/MultiThreading.java
@@ -10,8 +10,8 @@ public class MultiThreading{
 //        threadingExample2.run();
 
 //        for runnable
-        MultiThreadingWithRunnable multiThreadingWithRunnable = new MultiThreadingWithRunnable(1);
-        MultiThreadingWithRunnable multiThreadingWithRunnable2 = new MultiThreadingWithRunnable(2);
+        MultiThreadingWithRunnable multiThreadingWithRunnable = new MultiThreadingWithRunnable(0);
+        MultiThreadingWithRunnable multiThreadingWithRunnable2 = new MultiThreadingWithRunnable(1);
         Thread myThread = new Thread(multiThreadingWithRunnable);
         Thread myThread2 = new Thread(multiThreadingWithRunnable2);
         myThread.start();
@@ -19,21 +19,25 @@ public class MultiThreading{
 //        System.out.println(myThread.isAlive());
         myThread2.start();
 
+
+        // session
+
+
     }
 }
 
 
 class MultiThreadingExample extends Thread{
 
-    private int thredNum;
-    public MultiThreadingExample(int threadNum){
-        this.thredNum = threadNum;
+    private int rem;
+    public MultiThreadingExample(int rem){
+        this.rem = rem;
     }
 
     @Override
     public void run() {
         for(int i=1; i<=5; i++){
-            System.out.println(i+" from thread " + thredNum);
+            System.out.println(i+" from thread " + rem);
 //            if(thredNum == 2) throw new RuntimeException("Error occur at thread 2");
             try {
                 Thread.sleep(1000);
@@ -46,22 +50,24 @@ class MultiThreadingExample extends Thread{
 
 class MultiThreadingWithRunnable implements Runnable{
 
-    private int thredNum;
-    public MultiThreadingWithRunnable(int threadNum){
-        this.thredNum = threadNum;
+    private int rem;
+    public MultiThreadingWithRunnable(int rem){
+        this.rem = rem;
     }
 
     @Override
     public void run() {
-        for(int i=1; i<=5; i++){
-            System.out.println(i+" from thread " + thredNum);
-//            if(thredNum == 2) throw new RuntimeException("Error occur at thread 2");
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+
+            for (int i = 1; i <= 20; i++) {
+                if (i % 2 == rem) {
+                    System.out.println(i + " from thread " + rem);
+                }
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
             }
-        }
     }
 }
 
